5eb7fd5a3b7670cc31fd6a3d86b097281a294fe8
ltCAr1504 <carlosmiguel1101@gmail.com>
2025-08-21 01:54:08 -0400

    Changes 8/21/2025 1:53

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-import './style.css'
+import './style.css';
+
+const rootStyles = getComputedStyle(document.documentElement)
+const colorYellow = rootStyles.getPropertyValue('--color-yellow')
+const navbar = document.querySelector('.navbar')
+const links = document.querySelectorAll('#nav__link')
+
+navbar.addEventListener('mouseenter', () => {
+  navbar.style.backgroundColor = colorYellow
+  navbar.style.borderColor = colorYellow
+  links.forEach(l => {
+    l.style.color = 'white'
+  });
+})
+
+
+navbar.addEventListener('mouseleave', () => {
+  navbar.style.backgroundColor = '';
+  navbar.style.borderColor = ''
+  links.forEach(l => {
+    l.style.color = ''
+  })
+})
