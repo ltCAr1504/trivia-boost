59c32cd50ec69aa91863d3ab3c6d7505245931d9
ltCAr1504 <109382341+ltCAr1504@users.noreply.github.com>
2025-09-04 00:54:44 +0000

    Changes 9/3/2025 8:54

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 
 const rootStyles = getComputedStyle(document.documentElement)
-const colorYellow = rootStyles.getPropertyValue('--color-yellow')
+const colorGrayBlack = rootStyles.getPropertyValue('--color-grayBlack')
 const navbar = document.querySelector('.navbar')
 const links = document.querySelectorAll('#nav__link')
-const dropDownLinkBox = document.querySelector('.nav__dropdown')
 const dropDownLink = document.getElementById('nav__dropdown__link');
 const dropDownBox = document.querySelector('.nav__dropdown-menu')
-const historyLink = document.querySelector('.dropdown__link__history')
+
 
 
 
 navbar.addEventListener('mouseenter', () => {
-  navbar.style.backgroundColor = colorYellow
-  navbar.style.borderColor = colorYellow
+  navbar.style.backgroundColor = colorGrayBlack
+  navbar.style.borderColor = colorGrayBlack
   dropDownLink.style.color = 'white'
   links.forEach(l => {
     l.style.color = 'white'
@@ -50,17 +49,7 @@ dropDownBox.addEventListener('mouseleave', () => {
 })
 
 
-dropDownLinkBox.addEventListener('mouseenter', () => {
-  dropDownLink.classList.add('active')
-  navbar.style.borderBottomLeftRadius = '0px'
-  navbar.style.borderBottomRightRadius = '0px'
-})
 
 
-dropDownLinkBox.addEventListener('mouseleave', () => {
-  dropDownLink.classList.remove('active')
-  navbar.style.borderBottomLeftRadius = '10px'
-  navbar.style.borderBottomRightRadius = '10px'
-})
 
 
