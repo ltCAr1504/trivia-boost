b11ae7a395a154f86aebf01ef4038078200e260d
ltCAr1504 <109382341+ltCAr1504@users.noreply.github.com>
2025-09-05 18:31:25 +0000

    Changes

--- src/main.js
+++ src/main.js
@@ -1,34 +1,33 @@
 
-const rootStyles = getComputedStyle(document.documentElement)
-const colorGrayBlack = rootStyles.getPropertyValue('--color-grayBlack')
-const navbar = document.querySelector('.navbar')
-const links = document.querySelectorAll('#nav__link')
-const dropDownLink = document.getElementById('nav__dropdown__link');
-const dropDownBox = document.querySelector('.nav__dropdown-menu')
+const navDropdown = document.querySelector('.nav__dropdown-menu')
+const dropdownLink = document.getElementById('nav__dropdown__link')
 
 
+function displayDropdownMenu(){ 
+  if (navDropdown.style.opacity === '1'){
+    navDropdown.style.opacity = '0'
+  }else{
+    navDropdown.style.opacity = '1'
+  }                
+}
 
+dropdownLink.addEventListener('click',displayDropdownMenu)
 
-
-dropDownLink.addEventListener('mouseenter', () =>{
-  navbar.style.borderBottomLeftRadius = '0px'
-  navbar.style.borderBottomRightRadius = '0px'
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 5){
+    navDropdown.style.opacity = '0'
+  }
 })
 
-dropDownLink.addEventListener('mouseleave', () => {
-  navbar.style.borderBottomLeftRadius = '10px'
-  navbar.style.borderBottomRightRadius = '10px'
-})
 
-dropDownBox.addEventListener('mouseenter', () => {
-  navbar.style.borderBottomLeftRadius = '0px'
-  navbar.style.borderBottomRightRadius = '0px'
-})
 
-dropDownBox.addEventListener('mouseleave', () => {
-  navbar.style.borderBottomLeftRadius = '10px'
-  navbar.style.borderBottomRightRadius = '10px'
-})
+
+
+
+
+
+
+
 
 
 
