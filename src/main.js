8aa21918d99614dc3f54c6bb1c2cdffba438e14d
ltCAr1504 <109382341+ltCAr1504@users.noreply.github.com>
2025-09-06 04:30:11 +0000

    Changes 9/6/2025 12:30

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 
 const navDropdown = document.querySelector('.nav__dropdown-menu')
 const dropdownLink = document.getElementById('nav__dropdown__link')
+const arrowIcon = document.getElementById('arrow__img')
 
 
 function displayDropdownMenu(){ 
   if (navDropdown.style.opacity === '1'){
     navDropdown.style.opacity = '0'
+    arrowIcon.style.transform = 'rotate(0deg)'
   }else{
     navDropdown.style.opacity = '1'
+    arrowIcon.style.transform = 'rotate(-180deg)'
   }                
 }
 
@@ -16,8 +19,17 @@ dropdownLink.addEventListener('click',displayDropdownMenu)
 window.addEventListener('scroll', () => {
   if(window.scrollY > 5){
     navDropdown.style.opacity = '0'
+    arrowIcon.style.transform = 'rotate(0deg)'
   }
 })
+
+document.addEventListener('click', (e) => {
+  if (!navDropdown.contains(e.target) && e.target !== dropdownLink) {
+    navDropdown.style.opacity = '0'
+    arrowIcon.style.transform = 'rotate(0deg)'
+  }
+})
+
 
 
 
