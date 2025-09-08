679bb1169ee6e041b69fce99f14a4386d70b3263
Carlos <carlosmiguel1101@gmail.com>
2025-09-07 20:44:49 -0400

    Changes 9/7/2025 8:44

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 const navDropdown = document.querySelector('.nav__dropdown-menu')
 const dropdownLink = document.getElementById('nav__dropdown__link')
 const arrowIcon = document.getElementById('arrow__img')
+const quizDifficulty = document.querySelectorAll('.quiz__difficulty')
+
+
 
 
 function displayDropdownMenu(){ 
@@ -31,10 +34,18 @@ document.addEventListener('click', (e) => {
 })
 
 
+const colors = {
+  'Easy': '#2ecc71',   // verde
+  'Medium': '#f1c40f', // amarillo
+  'Hard': '#e74c3c'    // rojo
+};
 
-
-
-
+quizDifficulty.forEach(item => {
+  const difficulty = item.textContent;
+  if(colors[difficulty]){
+    item.style.color = colors[difficulty];
+  }
+});
 
 
 
