b9d180b7a48e7106d2fdf8c4b18666b503349c64
ltCAr1504 <109382341+ltCAr1504@users.noreply.github.com>
2025-10-09 19:30:21 +0000

    Changes 10/9/2025 2:30

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require('express')
 const app = express();
 const path = require('path');
-const router = require('./routes/pages')
-const morgan = require('morgan')
+const router = require('./routes/pages.js')
+const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
